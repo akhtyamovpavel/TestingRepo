312c553992b059308b3aee8bb9fa081d6e7c1076
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2019-03-12 15:03:16 +0300

    Changed order of variables in unittest conventions

--- PythonTesting/00-unittest/TreeUnitTest.py
+++ PythonTesting/00-unittest/TreeUnitTest.py
@@ -4,31 +4,31 @@ import unittest
 from TreeOpener import TreeOpener
 
 
-class MyTestCase(unittest.TestCase):
+class TreeUnitTest(unittest.TestCase):
     def test_tree_opener(self):
         tree_opener: TreeOpener = TreeOpener('assets')
-        self.assertSequenceEqual(tree_opener.run(), [
+        self.assertSequenceEqual([
             os.path.join('assets', '1.txt')
-        ])
+        ], tree_opener.run())
 
     def test_dir_opener(self):
         tree_opener: TreeOpener = TreeOpener('assets', with_dir=True)
-        self.assertSequenceEqual(tree_opener.run(), [
+        self.assertSequenceEqual([
             'assets',
             os.path.join('assets', '1.txt')
-        ])
+        ], tree_opener.run())
 
     def test_depths_two(self):
         tree_opener: TreeOpener = TreeOpener('assets_multiple', with_dir=True)
-        self.assertSequenceEqual(tree_opener.run(), [
+        self.assertSequenceEqual([
             'assets_multiple',
             os.path.join('assets_multiple', 'assets'),
             os.path.join('assets_multiple', 'assets', '1.txt')
-        ])
+        ], tree_opener.run())
 
     def test_not_directory(self):
         tree_opener: TreeOpener = TreeOpener('1.txt')
-        self.assertRaises(tree_opener.run(), ValueError)
+        self.assertRaises(ValueError, tree_opener.run)
 
 
 if __name__ == '__main__':
